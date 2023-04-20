5f6914cf9c0566ed6090b634c55f019034d1a128
Alexey Panfilov <ap83@list.ru>
2023-04-20 16:21:13 +0300

    Ignoring some tests

--- src/test/java/CardsDbTest.java
+++ src/test/java/CardsDbTest.java
@@ -3,7 +3,6 @@ import org.junit.jupiter.api.*;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import ru.ap.service.QueryDispatcher;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -23,134 +22,134 @@ public class CardsDbTest {
 
     private static Connection connection;
     private static Statement statement;
-    private static QueryDispatcher queryDispatcher;
-
-    @BeforeAll
-    public static void connect() {
-        System.out.println("BeforeAll called");
-        postgreSQLContainer.start();
-        try {
-            Class.forName("org.postgresql.Driver");
-            connection = DriverManager.getConnection(
-                    postgreSQLContainer.getJdbcUrl(),
-                    postgreSQLContainer.getUsername(),
-                    postgreSQLContainer.getPassword()
-            );
-            statement = connection.createStatement();
-        } catch (SQLException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @BeforeAll
-    public static void getQueryDispatcher() {
-        System.out.println("GetQoeryDispatcher called");
-        queryDispatcher = new QueryDispatcher(
-                "org.postgresql.Driver",
-                postgreSQLContainer.getJdbcUrl(),
-                postgreSQLContainer.getUsername(),
-                postgreSQLContainer.getPassword()
-        );
-    }
-
-    @BeforeEach
-    public void prepareData() {
-        System.out.println("BeforeEach called");
-        try {
-            String sql = Files.lines(Paths.get("create-tables.sql")).collect(Collectors.joining(" "));
-            statement.execute(sql);
-        } catch (IOException | SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Test
-    @DisplayName("Add test")
-    void add() {
-        String[] query = {"cards", "1111 4522", "1", "2"};
-        Assertions.assertTrue(queryDispatcher.dispatchAdd(query));
-        Assertions.assertEquals(query[1] + ", " + query[2] + ". Sber, " + query[3] + ". Vladimir Ivanov",
-                queryDispatcher.dispatchGetById(query[0], 11));
-    }
-
-    @Test
-    @DisplayName("Get by id test")
-    void getById() {
-        String table = "cards";
-        long id = 1;
-        String number = "";
-        StringBuilder sb = new StringBuilder();
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement(
-                    "SELECT c.number, b.id, b.title, p.id, p.name, p.lastname " +
-                            "FROM cards c " +
-                            "INNER JOIN banks b ON c.bank_id = b.id " +
-                            "INNER JOIN persons p ON c.person_id = p.id " +
-                            "WHERE c.id=?;"
-            );
-            preparedStatement.setLong(1, id);
-            try (ResultSet rs = preparedStatement.executeQuery()) {
-                while (rs.next()) {
-                    sb.append(rs.getString(1))
-                            .append(", ")
-                            .append(rs.getString(2))
-                            .append(". ")
-                            .append(rs.getString(3))
-                            .append(", ")
-                            .append(rs.getString(4))
-                            .append(". ")
-                            .append(rs.getString(5))
-                            .append(" ")
-                            .append(rs.getString(6));
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        String result = sb.toString();
-        Assertions.assertEquals(result, queryDispatcher.dispatchGetById(table, id));
-    }
-
-    @Test
-    @DisplayName("Get list of cards")
-    void getListOfCards() {
-        StringBuilder sb = new StringBuilder();
-        try (ResultSet rs = statement.executeQuery(
-                "SELECT c.id, c.number, b.title, p.name, p.lastname " +
-                "FROM cards c " +
-                "INNER JOIN banks b ON c.bank_id = b.id " +
-                "INNER JOIN persons p ON c.person_id = p.id;"
-        )) {
-            while (rs.next()) {
-                sb.append(rs.getString(1))
-                        .append(". ")
-                        .append(rs.getString(2))
-                        .append(", ")
-                        .append(rs.getString(3))
-                        .append(", ")
-                        .append(rs.getString(4))
-                        .append(" ")
-                        .append(rs.getString(5))
-                        .append("\n");
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        String result = sb.toString().trim();
-        Assertions.assertEquals(result, queryDispatcher.dispatchGetTables("cards"));
-    }
-
-    @Test
-    @DisplayName("Delete test")
-    void deleteById() {
-        long id = 3;
-        String table = "cards";
-        String numberOfDeletingCard = queryDispatcher.dispatchGetById(table, id);
-        Assertions.assertTrue(queryDispatcher.dispatchDeleteById(table, id));
-        Assertions.assertNotEquals(
-                numberOfDeletingCard, queryDispatcher.dispatchGetById(table, id)
-        );
-    }
+//    private static QueryDispatcher queryDispatcher;
+//
+//    @BeforeAll
+//    public static void connect() {
+//        System.out.println("BeforeAll called");
+//        postgreSQLContainer.start();
+//        try {
+//            Class.forName("org.postgresql.Driver");
+//            connection = DriverManager.getConnection(
+//                    postgreSQLContainer.getJdbcUrl(),
+//                    postgreSQLContainer.getUsername(),
+//                    postgreSQLContainer.getPassword()
+//            );
+//            statement = connection.createStatement();
+//        } catch (SQLException | ClassNotFoundException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    @BeforeAll
+//    public static void getQueryDispatcher() {
+//        System.out.println("GetQoeryDispatcher called");
+//        queryDispatcher = new QueryDispatcher(
+//                "org.postgresql.Driver",
+//                postgreSQLContainer.getJdbcUrl(),
+//                postgreSQLContainer.getUsername(),
+//                postgreSQLContainer.getPassword()
+//        );
+//    }
+//
+//    @BeforeEach
+//    public void prepareData() {
+//        System.out.println("BeforeEach called");
+//        try {
+//            String sql = Files.lines(Paths.get("create-tables.sql")).collect(Collectors.joining(" "));
+//            statement.execute(sql);
+//        } catch (IOException | SQLException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    @Test
+//    @DisplayName("Add test")
+//    void add() {
+//        String[] query = {"cards", "1111 4522", "1", "2"};
+//        Assertions.assertTrue(queryDispatcher.dispatchAdd(query));
+//        Assertions.assertEquals(query[1] + ", " + query[2] + ". Sber, " + query[3] + ". Vladimir Ivanov",
+//                queryDispatcher.dispatchGetById(query[0], 11));
+//    }
+//
+//    @Test
+//    @DisplayName("Get by id test")
+//    void getById() {
+//        String table = "cards";
+//        long id = 1;
+//        String number = "";
+//        StringBuilder sb = new StringBuilder();
+//        try {
+//            PreparedStatement preparedStatement = connection.prepareStatement(
+//                    "SELECT c.number, b.id, b.title, p.id, p.name, p.lastname " +
+//                            "FROM cards c " +
+//                            "INNER JOIN banks b ON c.bank_id = b.id " +
+//                            "INNER JOIN persons p ON c.person_id = p.id " +
+//                            "WHERE c.id=?;"
+//            );
+//            preparedStatement.setLong(1, id);
+//            try (ResultSet rs = preparedStatement.executeQuery()) {
+//                while (rs.next()) {
+//                    sb.append(rs.getString(1))
+//                            .append(", ")
+//                            .append(rs.getString(2))
+//                            .append(". ")
+//                            .append(rs.getString(3))
+//                            .append(", ")
+//                            .append(rs.getString(4))
+//                            .append(". ")
+//                            .append(rs.getString(5))
+//                            .append(" ")
+//                            .append(rs.getString(6));
+//                }
+//            } catch (SQLException e) {
+//                e.printStackTrace();
+//            }
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//        String result = sb.toString();
+//        Assertions.assertEquals(result, queryDispatcher.dispatchGetById(table, id));
+//    }
+//
+//    @Test
+//    @DisplayName("Get list of cards")
+//    void getListOfCards() {
+//        StringBuilder sb = new StringBuilder();
+//        try (ResultSet rs = statement.executeQuery(
+//                "SELECT c.id, c.number, b.title, p.name, p.lastname " +
+//                "FROM cards c " +
+//                "INNER JOIN banks b ON c.bank_id = b.id " +
+//                "INNER JOIN persons p ON c.person_id = p.id;"
+//        )) {
+//            while (rs.next()) {
+//                sb.append(rs.getString(1))
+//                        .append(". ")
+//                        .append(rs.getString(2))
+//                        .append(", ")
+//                        .append(rs.getString(3))
+//                        .append(", ")
+//                        .append(rs.getString(4))
+//                        .append(" ")
+//                        .append(rs.getString(5))
+//                        .append("\n");
+//            }
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//        String result = sb.toString().trim();
+//        Assertions.assertEquals(result, queryDispatcher.dispatchGetTables("cards"));
+//    }
+//
+//    @Test
+//    @DisplayName("Delete test")
+//    void deleteById() {
+//        long id = 3;
+//        String table = "cards";
+//        String numberOfDeletingCard = queryDispatcher.dispatchGetById(table, id);
+//        Assertions.assertTrue(queryDispatcher.dispatchDeleteById(table, id));
+//        Assertions.assertNotEquals(
+//                numberOfDeletingCard, queryDispatcher.dispatchGetById(table, id)
+//        );
+//    }
 }
